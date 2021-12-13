fe7e4cf824bc6dfee5a68506658487c8e7876e03
johnmart19 <johnivan19999@gmail.com>
2021-12-13 11:32:37 +0200

    alioth: Welcome to PixelPlus-UI

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,22 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-EVO_BUILD_TYPE := OFFICIAL
+# Inherit some common PixelPlus-UI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 WITH_GAPPS := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# Maintainer Props
-EVO_MAINTAINER := DarkAngelGR
-EVO_SUPPORT_URL := https://t.me/EvolutionXAlioth
-EVO_DONATE_URL := https://www.paypal.me/asavvo01
+# PixelPlus-UI Official Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+PPUI_MAINTAINER := johnmart19
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
