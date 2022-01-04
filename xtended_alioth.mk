97427df2b4a3b2d0687a5f42840ae6854c3e5dff
mady51 <p.mschary@gmail.com>
2022-01-04 17:00:52 +0530

    adapt to Xtended

--- xtended_alioth.mk
+++ xtended_alioth.mk
@@ -8,15 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common aosp stuffs.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
-TARGET_USES_BLUR := true
-#USE_PIXEL_CHARGING := true
-#USE_AOSP_CLOCK := true
-PRODUCT_NAME := aosp_alioth
+
+PRODUCT_NAME := xtended_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
