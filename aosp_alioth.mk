fe4918446abc8533ec68ae7e59d1fe72456cc106
Eidoron1 <eidoronp67@gmail.com>
2022-01-07 02:50:45 +0545

    alioth:Arcana Bringup

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuffs.
+# Inherit some common aosp stuffs.
 $(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from alioth device
@@ -16,7 +16,7 @@ $(call inherit-product, device/xiaomi/alioth/device.mk)
 TARGET_USES_BLUR := true
 #USE_PIXEL_CHARGING := true
 #USE_AOSP_CLOCK := true
-PRODUCT_NAME := xtended_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
