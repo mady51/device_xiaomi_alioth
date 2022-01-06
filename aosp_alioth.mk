52a7e222bdb20edf4859142b10182b386dc14f36
Eidoron1 <eidoronp67@gmail.com>
2022-01-06 19:22:56 +0545

    alioth:Arcana Bringup

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,14 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common xtended configurations.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common lineage stuffs.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 TARGET_USES_BLUR := true
 #USE_PIXEL_CHARGING := true
-TARGET_BOOT_ANIMATION_RES := 1080
 #USE_AOSP_CLOCK := true
 PRODUCT_NAME := xtended_alioth
 PRODUCT_DEVICE := alioth
@@ -23,10 +22,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-#GAPPS
+ARCANA_DEVICE:= alioth
+ARCANA_MAINTAINER := Eidoron1
 WITH_GAPPS := true
-#XTRA_GAPPS
-TARGET_INLCUDE_EXTRA_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
